807921fab90267977c2a10e8f9ac3b46e96339a6
Sujal <spate290@uic.edu>
2015-03-12 22:09:15 -0500

    LoginServlet and Datebase

---
+++ code/src/databases/ConnectionManager.java
@@ -0,0 +1,27 @@
+package databases;
+
+import java.sql.*;
+
+public class ConnectionManager {
+
+	static Connection con;
+	static String url;
+
+	public static Connection getConnection() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			try {
+				con = DriverManager.getConnection(
+						"jdbc:postgresql://localhost:5432/SVM", "postgres",
+						"cs422");
+			}
+			catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+}
